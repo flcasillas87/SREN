d623ad9b2abbeec9e1cf66b91c603e7815b01a0d
Fernando Lopez Casillas <flcasillas@gmail.com>
2025-12-23 11:14:37 -0600

    flc

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,62 @@
-import { ChangeDetectionStrategy,Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { material_modules } from '@core/providers/material.provider';
+import { AuthService } from '@services/auth.service';
+import { AuthError } from '@supabase/supabase-js';
+
+interface SignUpForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  standalone: true,
+  imports: [material_modules, ReactiveFormsModule, RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class AuthSignUp {
+export default class SignUpComponent {
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
 
+  public readonly isLoading = signal(false);
+
+  public readonly signUpForm = this._formBuilder.group<SignUpForm>({
+    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(6)] }),
+  });
+
+  public async submit(): Promise<void> {
+    if (this.signUpForm.invalid) return;
+
+    this.isLoading.set(true);
+
+    try {
+      const { email, password } = this.signUpForm.getRawValue();
+      
+      const { data, error } = await this._authService.signUp({ email, password });
+
+      if (error) throw error;
+
+      if (data.user && !data.session) {
+        alert('Registro exitoso. Revisa tu correo.');
+        this._router.navigateByUrl('/auth/login');
+      } else {
+        this._router.navigateByUrl('/');
+      }
+
+    } catch (error: unknown) {
+      let message = 'Error inesperado';
+      if (error instanceof AuthError) message = error.message;
+      else if (error instanceof Error) message = error.message;
+      
+      console.error('[SignUpComponent]:', message);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
 }
