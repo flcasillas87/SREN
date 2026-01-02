845c3c7050564a13361780ef835dbbf8a1f45707
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2026-01-01 23:30:28 -0600

    FLC

---
+++ src/app/features/centrales/components/central-table/central-table.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-central-table',
+  imports: [],
+  templateUrl: './central-table.html',
+  styleUrl: './central-table.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CentralTableComponent {
+
+}
