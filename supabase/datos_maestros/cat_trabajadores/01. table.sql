4768e895b7553c544f17e5c00e9e34121c8e06c5
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2026-01-24 01:08:38 -0600

    flc

--- supabase/datos_maestros/cat_trabajadores/01. table.sql
+++ supabase/datos_maestros/cat_trabajadores/01. table.sql
@@ -2,19 +2,22 @@
 -- Tabla: Catálogo de Trabajadores
 -- =========================================================
 DROP TABLE IF EXISTS datos_maestros.cat_trabajadores CASCADE;
-
 CREATE TABLE datos_maestros.cat_trabajadores (
     id_trabajador uuid PRIMARY KEY DEFAULT extensions.uuid_generate_v4(),
-    rfc text UNIQUE NOT NULL,
-    curp text UNIQUE NOT NULL,
+    rfc text UNIQUE,
+    curp text UNIQUE,
     rpe text UNIQUE,
     nombre text NOT NULL,
-    apellido_paterno text NOT NULL,
+    apellido_paterno text,
     apellido_materno text,
+    tipo_contrato text,
+    puesto text,
     correo_electronico text UNIQUE,
     fecha_ingreso date,
     activo bool DEFAULT true,
-    
+    salario numeric(15, 2),
+    grupo_organico text,
+    nivel_desempeno text,
     created_at timestamptz DEFAULT now(),
     updated_at timestamptz DEFAULT now()
 );
